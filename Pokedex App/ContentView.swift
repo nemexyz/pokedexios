afcbc8df12090ca9c503ed115ddd6931bb07b670
Axel Cantor <nemexyz@gmail.com>
2021-02-05 13:34:32 -0500

    Initial Commit

---
+++ Pokedex App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pokedex App
+//
+//  Created by Axel Cantor on 5/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pokedex App/Pokedex_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pokedex_AppApp.swift
+//  Pokedex App
+//
+//  Created by Axel Cantor on 5/02/21.
+//
+
+import SwiftUI
+
+@main
+struct Pokedex_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
